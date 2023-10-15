f4b99cd489898622e5d734052fa0fc5f829a82db
flopox <l.llop@alumno.etec.um.edu.ar>
2023-10-15 01:02:11 -0300

    more detailed information for any user

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Human-recognizer
 An innovative application designed for efficient and secure user identification and authentication. Using advanced biometric recognition techniques, this tool provides a reliable method for verifying people's identities through unique features such as fingerprints, facial recognition, and voice recognition. 
+
+OpenCV-Python is employed for real-time human detection in a video. Here's a concise technical overview:
+
+Video Input: 
+OpenCV is used to initialize video capture from a file.
+
+Haar Cascade Classifier: 
+A pre-trained Haar Cascade classifier for full-body detection is loaded.
+
+Frame Processing Loop: 
+Frames from the video are processed sequentially.
+
+Object Detection: 
+The Haar Cascade classifier is applied to identify human bodies.
+
+Visual Feedback: 
+Detected human bodies are marked with rectangles.
+
+Real-time Display: 
+Frames with detections are displayed continuously.
+
+User Interaction: 
+The program exits upon detecting the 'q' key press.
--- human-recognizer.py
+++ human-recognizer.py
@@ -3,6 +3,7 @@ import cv2
 #Insert the name of the video you want the program to execute
 cap = cv2.VideoCapture('people_-_6387 (720p).mp4')
 
+#Using OpenCV to detect bodies
 human_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
 while(True):
@@ -13,7 +14,15 @@ while(True):
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    humans = human_cascade.detectMultiScale(gray, 1.2, 3)
+    humans = human_cascade.detectMultiScale(gray, 1.05, 2) 
+    # The value "1.05": A smaller value, such as 1.05 or 1.1, will cause objects 
+    # to be searched for in a wider range of sizes, but it can also make detection 
+    # slower and less accurate. A larger value, such as 1.2 or 1.3, will make 
+    # detection faster but potentially less sensitive to objects of different sizes.
+
+    # The value "2": It will require a greater match between rectangles before 
+    # considering an object. A lower value will make detection more sensitive 
+    # but can also generate more false positives.
 
     for (x, y, w, h) in humans:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
